78abfd3368f470557e816f51c2e3484b48692eed
adam <updega2@berkeley.edu>
2017-03-16 21:11:44 -0700

    DOC: doc for generalized polycube

--- Modules/Parameterization/vtkSVGeneralizedPolycube.cxx
+++ Modules/Parameterization/vtkSVGeneralizedPolycube.cxx
@@ -1,34 +1,50 @@
 /*=========================================================================
+ *
+ * Copyright (c) 2014 The Regents of the University of California.
+ * All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *=========================================================================*/
 
-  Program:   Visualization Toolkit
-  Module:    vtkSVGeneralizedPolycube.cxx
-
-  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-  All rights reserved.
-  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notice for more information.
-
-=========================================================================*/
 #include "vtkSVGeneralizedPolycube.h"
 
-#include "vtkCellArray.h"
 #include "vtkCellData.h"
+#include "vtkIdList.h"
 #include "vtkMath.h"
 #include "vtkPointData.h"
 #include "vtkSmartPointer.h"
 #include "vtkSVGlobals.h"
 
+// ----------------------
+// StandardNewMacro
+// ----------------------
 vtkStandardNewMacro(vtkSVGeneralizedPolycube);
 
-//---------------------------------------------------------------------------
-/**
- * @brief
- * @param *pd
- * @return
- */
+// ----------------------
+// Constructor
+// ----------------------
 vtkSVGeneralizedPolycube::vtkSVGeneralizedPolycube()
 {
   this->SurgeryLines = vtkPolyData::New();
@@ -68,12 +84,9 @@ vtkSVGeneralizedPolycube::vtkSVGeneralizedPolycube()
   this->GetCellData()->AddArray(rightNormals);
 }
 
-//---------------------------------------------------------------------------
-/**
- * @brief
- * @param *pd
- * @return
- */
+// ----------------------
+// Destructor
+// ----------------------
 vtkSVGeneralizedPolycube::~vtkSVGeneralizedPolycube()
 {
   if (this->SurgeryLines != NULL)
@@ -83,151 +96,152 @@ vtkSVGeneralizedPolycube::~vtkSVGeneralizedPolycube()
   }
 }
 
-//---------------------------------------------------------------------------
-/**
- * @brief
- * @param *pd
- * @return
- */
+// ----------------------
+// PrintSelf
+// ----------------------
 void vtkSVGeneralizedPolycube::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os, indent);
+
+  os << indent << "Number of grids: " << this->GetNumberOfCells() << "\n";
 }
 
-//---------------------------------------------------------------------------
-/**
- * @brief
- * @param *pd
- * @return
- */
+// ----------------------
+// Initialize
+// ----------------------
 void vtkSVGeneralizedPolycube::Initialize()
 {
   this->Superclass::Initialize();
 }
 
-//---------------------------------------------------------------------------
-/**
- * @brief
- * @param *pd
- * @return
- */
+// ----------------------
+// SetNumberOfGrids
+// ----------------------
 void vtkSVGeneralizedPolycube::SetNumberOfGrids(const int numberOfGrids)
 {
+  // Make sure we aren't trying to decrease the size
   int numCurrentGrids = this->GetNumberOfGrids();
   if (numberOfGrids <= numCurrentGrids)
   {
     vtkErrorMacro("Can only increase the number of grids, not decrease");
     return;
   }
+
+  // Set up blank ids from points of cells
   vtkNew(vtkIdList, blankIds);
   blankIds->SetNumberOfIds(8);
   for (int i=0; i<8; i++)
-  {
     blankIds->SetId(i, 0);
-  }
+
+  // Loop through new number of grids
   for (int i=numCurrentGrids; i <numberOfGrids; i++)
   {
+    // Set new cell and data on new cell
     this->InsertNextCell(VTK_HEXAHEDRON, blankIds);
     this->GetCellData()->GetArray("CubeType")->InsertNextTuple1(-1);
     this->GetCellData()->GetArray("ParentDirection")->InsertNextTuple1(-1);
     this->GetCellData()->GetArray("ChildDirection")->InsertNextTuple1(-1);
+
+    // Set blank cornder ids
     double blankCorner[8];
     for (int i=0; i<8; i++)
-    {
       blankCorner[i] = -1;
-    }
+
+    // Set more cell data
     this->GetCellData()->GetArray("CornerPtIds")->InsertNextTuple(blankCorner);
     double topNormal[3]; topNormal[0] = 0.0; topNormal[1] = 0.0; topNormal[2] = 1.0;
     this->GetCellData()->GetArray("TopNormal")->InsertNextTuple(topNormal);
     double rightNormal[3]; rightNormal[0] = 1.0; rightNormal[1] = 0.0; rightNormal[2] = 0.0;
     this->GetCellData()->GetArray("RightNormal")->InsertNextTuple(rightNormal);
   }
+
+  // We modified
   this->Modified();
 }
 
-//---------------------------------------------------------------------------
-/**
- * @brief
- * @param *pd
- * @return
- */
+// ----------------------
+// GetNumberOfGrids
+// ----------------------
 int vtkSVGeneralizedPolycube::GetNumberOfGrids()
 {
   return this->GetNumberOfCells();
 }
 
-//---------------------------------------------------------------------------
-/**
- * @brief
- * @param *pd
- * @return
- */
+// ----------------------
+// InsertGridWithCenter
+// ----------------------
 int vtkSVGeneralizedPolycube::InsertGridWithCenter(const int cellId, const double center[3], const double dims[3], const int cubetype, const int parentdirection, const int childdirection)
 {
+  // Inserting, must allocate space if not enough already.
   if (cellId >= this->GetNumberOfCells())
-  {
     this->SetNumberOfGrids(cellId);
-  }
+
+  // Now that there is space, set that grid
   this->SetGridWithCenter(cellId, center, dims, cubetype, parentdirection, childdirection);
 
   return SV_OK;
 }
 
-//---------------------------------------------------------------------------
-/**
- * @brief
- * @param *pd
- * @return
- */
+// ----------------------
+// SetGridWithCenter
+// ----------------------
 int vtkSVGeneralizedPolycube::SetGridWithCenter(const int cellId, const double center[3], const double dims[3], const int cubetype, const int parentdirection, const int childdirection)
 {
+  // Set the origin based on the center and the dimensions given
   double origin[3];
-
   for (int i=0; i<3; i++)
-  {
     origin[i] = center[i] - dims[i]/2.0;
-  }
 
+  // Set the gtid now
   this->SetGridWithOrigin(cellId, origin, dims, cubetype, parentdirection, childdirection);
   return SV_OK;
 }
 
-//---------------------------------------------------------------------------
-/**
- * @brief
- * @param *pd
- * @return
- */
+// ----------------------
+// InsertGridWithCenter
+// ----------------------
 int vtkSVGeneralizedPolycube::InsertGridWithOrigin(const int cellId, const double origin[3], const double dims[3], const int cubetype, const int parentdirection, const int childdirection)
 {
+  // Inserting, must allocate space if not enough already.
   if (cellId >= this->GetNumberOfCells())
-  {
     this->SetNumberOfGrids(cellId);
-  }
+
+  // Now that there is space, set that grid
   this->SetGridWithOrigin(cellId, origin, dims, cubetype, parentdirection, childdirection);
 
   return SV_OK;
 }
-//---------------------------------------------------------------------------
-/**
- * @brief
- * @param *pd
- * @return
- */
+
+// ----------------------
+// SetGridWithCenter
+// ----------------------
 int vtkSVGeneralizedPolycube::SetGridWithOrigin(const int cellId, const double origin[3], const double dims[3], const int cubetype, const int parentdirection, const int childdirection, const double topNormal[3], const double rightNormal[3], const int corners[8])
 {
+  // Set cell data for vectors
   this->GetCellData()->GetArray("TopNormal")->SetTuple(cellId, topNormal);
   this->GetCellData()->GetArray("RightNormal")->SetTuple(cellId, rightNormal);
+
+  // Loop through and set corner pt ids
   for (int i=0; i<8; i++)
-  {
     this->GetCellData()->GetArray("CornerPtIds")->SetComponent(cellId, i, corners[i]);
-  }
+
+  // Set the grid
   this->SetGridWithOrigin(cellId, origin, dims, cubetype, parentdirection, childdirection);
 
   return SV_OK;
 }
 
+// ----------------------
+// SetGridWithCenter
+// ----------------------
 /**
+ * \details When setting the cube, we need to make sure they are ordered in
+ * a certain way for the point data to actually correspond to something in the
+ * physical model. This is how we order the nodes.
+ * \verbatim
+ *       z  y
+ *       | /
+ *       0 -- x
  *               2---------1
  *              /|        /|
  *             / |       / |
@@ -237,30 +251,24 @@ int vtkSVGeneralizedPolycube::SetGridWithOrigin(const int cellId, const double o
  *            | /       | /
  *            |/        |/
  *            7---------4
- */
-//---------------------------------------------------------------------------
-/**
- * @brief
- * @param *pd
- * @return
- */
+ * \endverbatim  */
 int vtkSVGeneralizedPolycube::SetGridWithOrigin(const int cellId, const double origin[3], const double dims[3], const int cubetype, const int parentdirection, const int childdirection)
 {
+  // Set up the points
   vtkNew(vtkPoints, points);
   points->SetNumberOfPoints(8);
 
+  // Get default zero value to get all of our points in a nice array
   double pts[8][3];
   double zero[3];
   for (int i=0; i<3; i++)
-  {
     zero[i] = 0.0;
-  }
 
+  // Get all pts
   for (int i=0; i<8; i++)
-  {
     vtkMath::Add(origin, zero, pts[i]);
-  }
 
+  // Set the points according to the point layout
   pts[4][0] = origin[0] + dims[0];
 
   pts[5][0] = origin[0] + dims[0];
@@ -280,41 +288,38 @@ int vtkSVGeneralizedPolycube::SetGridWithOrigin(const int cellId, const double o
   pts[2][1] = origin[1] + dims[1];
   pts[2][2] = origin[2] + dims[2];
 
+  // Set the points in a vtkPoints
   for (int i=0; i<8; i++)
-  {
     points->SetPoint(i, pts[i]);
-  }
 
+  // Set the grid
   this->SetGrid(cellId, points, cubetype, parentdirection, childdirection);
 
   return SV_OK;
 }
 
-//---------------------------------------------------------------------------
-/**
- * @brief
- * @param *pd
- * @return
- */
+// ----------------------
+// InsertGrid
+// ----------------------
 int vtkSVGeneralizedPolycube::InsertGrid(const int cellId, vtkPoints *points, const int cubetype, const int parentdirection, const int childdirection)
 {
+  // Inserting, must allocate space if not enough already.
   if (cellId >= this->GetNumberOfCells())
-  {
     this->SetNumberOfGrids(cellId);
-  }
+
+  // Now that there is space, set that grid
   this->SetGrid(cellId, points, cubetype, parentdirection, childdirection);
 
   return SV_OK;
 }
 
-//---------------------------------------------------------------------------
-/**
- * @brief
- * @param *pd
- * @return
- */
+// ----------------------
+// SetGridWith
+// ----------------------
+// \details This is the one that actually sets the points!
 int vtkSVGeneralizedPolycube::SetGrid(const int cellId, vtkPoints *points, const int cubetype, const int parentdirection, const int childdirection)
 {
+  // Check to see if the right number of points given
   int numPts = points->GetNumberOfPoints();
   if (numPts != 8)
   {
@@ -322,25 +327,33 @@ int vtkSVGeneralizedPolycube::SetGrid(const int cellId, vtkPoints *points, const
     return SV_ERROR;
   }
 
+  // Get the inital point id for the cell to reference
   vtkIdType pointIds[8];
   vtkIdType npts = 8;
   int numPCPts = this->GetNumberOfPoints();
 
+  // Loop through points
   for (int i=0; i<numPts; i++)
   {
+    // Insert a new point with data
     this->GetPoints()->InsertNextPoint(points->GetPoint(i));
     this->GetPointData()->GetArray("CornerPtIds")->InsertNextTuple1(-1.0);
+
+    // Increment our point reference
     pointIds[i] = numPCPts++;
   }
+
+  // If this is less than number of cells, we gucci
   if (cellId < this->GetNumberOfCells())
-  {
     this->ReplaceCell(cellId, npts, pointIds);
-  }
+  // otherwise, we need to get upset
   else
   {
     vtkErrorMacro("CellId is greater than number of cells, must set number of grids to correct size first");
     return SV_ERROR;
   }
+
+  // Add cell data
   this->GetCellData()->GetArray("CubeType")->SetTuple1(cellId, cubetype);
   this->GetCellData()->GetArray("ParentDirection")->SetTuple1(cellId, parentdirection);
   this->GetCellData()->GetArray("ChildDirection")->SetTuple1(cellId, childdirection);
