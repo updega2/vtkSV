e7cf82ded0844fae43b0ff34d2582d26ad983a8f
updega2 <updega2@berkeley.edu>
2017-04-12 10:10:49 -0700

    MAINT: Adding travis

---
+++ travis/travis_build_vtk.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+MAKE="make --jobs=$NUM_THREADS --keep-going"
+
+cmake_arg_str=" -DBUILD_TESTING=1 -DBUILD_SHARED_LIBS=1  -DBUILD_EXAMPLES=0 "
+vtk_repo_str=""
+if [ "$VTK_VERSION" == "6.0" ]; then
+    vtk_repo_str=" --branch v6.0.0"
+elif [ "$VTK_VERSION" == "6.2" ]; then
+    vtk_repo_str=" --branch v6.2.0"
+elif [ "$VTK_VERSION" == "7.0" ]; then
+    vtk_repo_str=" --branch v7.0.0"
+fi
+if [ -d $VTK_SOURCE_DIR ]; then
+    echo $VTK_SOURCE_DIR exists
+    if [ ! -f $VTK_SOURCE_DIR/CMakeLists.txt ]; then
+        echo $VTK_SOURCE_DIR does not contain CMakeList.txt
+        rm -rf $VTK_SOURCE_DIR
+    fi
+fi
+if [ ! -d "$VTK_SOURCE_DIR" ]; then
+    git clone $vtk_repo_str $VTK_SOURCE_DIR
+fi
+mkdir -p $VTK_DIR
+cd $VTK_DIR
+cmake $cmake_arg_str $VTK_SOURCE_DIR
+$MAKE
