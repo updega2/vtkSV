21015f81815347d693f447f4bd786a77e166860f
updega2 <updega2@berkeley.edu>
2017-04-12 10:24:18 -0700

    MAINT: need repo

--- travis/travis_build_vtk.sh
+++ travis/travis_build_vtk.sh
@@ -3,14 +3,15 @@
 set -e
 MAKE="make --jobs=$NUM_THREADS --keep-going"
 
-cmake_arg_str=" -DBUILD_TESTING=1 -DBUILD_SHARED_LIBS=1  -DBUILD_EXAMPLES=0 "
+cmake_arg_str=" -DBUILD_TESTING=1 -DBUILD_SHARED_LIBS=1  -DBUILD_EXAMPLES=0"
+vtk_repo="https://github.com/Kitware/VTK.git"
 vtk_repo_str=""
 if [ "$VTK_VERSION" == "6.0" ]; then
-    vtk_repo_str=" --branch v6.0.0"
+    vtk_repo_str=" --branch v6.0.0 $vtk_repo"
 elif [ "$VTK_VERSION" == "6.2" ]; then
-    vtk_repo_str=" --branch v6.2.0"
+    vtk_repo_str=" --branch v6.2.0 $vtk_repo"
 elif [ "$VTK_VERSION" == "7.0" ]; then
-    vtk_repo_str=" --branch v7.0.0"
+    vtk_repo_str=" --branch v7.0.0 $vtk_repo"
 fi
 if [ -d $VTK_SOURCE_DIR ]; then
     echo $VTK_SOURCE_DIR exists
