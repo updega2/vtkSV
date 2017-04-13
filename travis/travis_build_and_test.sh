332fd269d027536fe00bee46a4187798f5691e67
updega2 <updega2@berkeley.edu>
2017-04-13 09:25:47 -0700

    Remove 6.0, update sudo

--- travis/travis_build_and_test.sh
+++ travis/travis_build_and_test.sh
@@ -4,7 +4,8 @@ set -e
 MAKE="make --jobs=$NUM_THREADS --keep-going"
 MAKE_TEST=ctest
 
-mkdir -p $BUILD_DIR
+sudo mkdir -p $BUILD_DIR
+sudo chmod -R 755 $BUILD_DIR
 cd $BUILD_DIR
 CMAKE_BUILD_ARGS="$CMAKE_BUILD_ARGS -DVTK_DIR:PATH=$VTK_DIR -DBUILD_TESTING:BOOL=ON"
 echo CMAKE_BUILD_ARGS: $CMAKE_BUILD_ARGS
--- travis/travis_build_vtk.sh
+++ travis/travis_build_vtk.sh
@@ -6,9 +6,7 @@ MAKE="make --jobs=$NUM_THREADS --keep-going"
 cmake_arg_str=" -DBUILD_TESTING=1 -DBUILD_SHARED_LIBS=1  -DBUILD_EXAMPLES=0"
 vtk_repo="https://github.com/Kitware/VTK.git"
 vtk_repo_str=""
-if [ "$VTK_VERSION" == "6.0" ]; then
-    vtk_repo_str=" --branch v6.0.0 $vtk_repo"
-elif [ "$VTK_VERSION" == "6.2" ]; then
+if [ "$VTK_VERSION" == "6.2" ]; then
     vtk_repo_str=" --branch v6.2.0 $vtk_repo"
 elif [ "$VTK_VERSION" == "7.0" ]; then
     vtk_repo_str=" --branch v7.0.0 $vtk_repo"
@@ -24,7 +22,8 @@ if [ ! -d "$VTK_SOURCE_DIR" ]; then
     echo "Using git to clone source"
     git clone $vtk_repo_str $VTK_SOURCE_DIR
 fi
-mkdir -p $VTK_DIR
+sudo mkdir -p $VTK_DIR
+sudo chmod -R 755 $VTK_DIR
 cd $VTK_DIR
 cmake $cmake_arg_str $VTK_SOURCE_DIR
 $MAKE
