65e6ad7aef9123121039f5a4e55094fd7d3ad431
updega2 <updega2@berkeley.edu>
2018-05-23 08:49:16 -0700

    DEV: new upde smoothing

--- Modules/Geometry/vtkSVUpdeSmoothing.cxx
+++ Modules/Geometry/vtkSVUpdeSmoothing.cxx
@@ -68,6 +68,7 @@ vtkStandardNewMacro(vtkSVUpdeSmoothing);
 vtkSVUpdeSmoothing::vtkSVUpdeSmoothing()
 {
   this->WorkPd = vtkPolyData::New();
+  this->SourcePd = NULL;
 
   this->NumberOfOuterSmoothOperations = 1;
   this->NumberOfInnerSmoothOperations = 500;
@@ -86,6 +87,12 @@ vtkSVUpdeSmoothing::~vtkSVUpdeSmoothing()
   {
     this->WorkPd->Delete();
     this->WorkPd = NULL;
+  }
+
+  if (this->SourcePd != NULL)
+  {
+    this->SourcePd->Delete();
+    this->SourcePd = NULL;
   }
 
   if (this->SmoothPointArrayName != NULL)
@@ -143,6 +150,13 @@ int vtkSVUpdeSmoothing::RequestData(vtkInformation *vtkNotUsed(request),
 
   vtkSVGeneralUtils::GiveIds(this->WorkPd, "TmpInternalIds");
 
+  // Build locator if source given
+  vtkNew(vtkCellLocator, cellLocator);
+  if (this->SourcePd != NULL)
+  {
+    cellLocator->SetDataSet(this->SourcePd);
+    cellLocator->BuildLocator();
+  }
 
   // Set fixed points
   std::vector<int> fixedPoints(numPts, 0);
@@ -167,6 +181,40 @@ int vtkSVUpdeSmoothing::RequestData(vtkInformation *vtkNotUsed(request),
       }
     }
   }
+
+
+  // ========================= FEATURE EDGES ===============================
+  vtkNew(vtkPolyData, idPd);
+  idPd->DeepCopy(this->WorkPd);
+  vtkSVGeneralUtils::GiveIds(idPd, "TmpInternalIds");
+
+  //this->SetSmoothPointArrayName("SmoothPoints");
+  vtkNew(vtkIntArray, tmpSmoothPointArray);
+  tmpSmoothPointArray->SetNumberOfTuples(idPd->GetNumberOfPoints());
+  tmpSmoothPointArray->FillComponent(0, 1);
+  tmpSmoothPointArray->SetName("SmoothPoints");
+
+  vtkNew(vtkFeatureEdges, featurer);
+  featurer->SetInputData(idPd);
+  featurer->NonManifoldEdgesOff();
+  featurer->ManifoldEdgesOff();
+  featurer->BoundaryEdgesOff();
+  featurer->FeatureEdgesOn();
+  featurer->SetFeatureAngle(70.0);
+  featurer->Update();
+
+  vtkNew(vtkPolyData, featurePd);
+  featurePd->DeepCopy(featurer->GetOutput());
+
+  int realPtId;
+  for (int i=0; i<featurePd->GetNumberOfPoints(); i++)
+  {
+    realPtId = featurePd->GetPointData()->GetArray("TmpInternalIds")->GetTuple1(i);
+    fixedPoints[realPtId] = 1;
+    tmpSmoothPointArray->SetTuple1(realPtId, 0);
+  }
+  this->WorkPd->GetPointData()->AddArray(tmpSmoothPointArray);
+  // ========================= FEATURE EDGES ===============================
 
   vtkIdType npts, *pts;
   vtkNew(vtkIdList, pointCellIds);
@@ -232,198 +280,240 @@ int vtkSVUpdeSmoothing::RequestData(vtkInformation *vtkNotUsed(request),
 
     vtkNew(vtkSVLocalSmoothPolyDataFilter, smoother);
     smoother->SetInputData(tmp);
-    smoother->SetUsePointArray(1);
-    smoother->SetSmoothPointArrayName(this->SmoothPointArrayName);
     smoother->SetNumberOfIterations(this->NumberOfInnerSmoothOperations);
+    if (this->SmoothPointArrayName != NULL)
+    {
+      smoother->SetUsePointArray(1);
+      smoother->SetSmoothPointArrayName(this->SmoothPointArrayName);
+    }
+    //if (this->SourcePd != NULL)
+    //{
+    //  smoother->SetSourceData(this->SourcePd);
+    //}
     smoother->Update();
 
-    vtkNew(vtkPolyDataNormals, tmpNormaler);
-    tmpNormaler->SetInputData(tmp);
-    tmpNormaler->SplittingOff();
-    tmpNormaler->ComputeCellNormalsOn();
-    tmpNormaler->ComputePointNormalsOn();
-    tmpNormaler->Update();
+    tmp->DeepCopy(smoother->GetOutput());
 
-    vtkDataArray *tPtNormals =
-      tmpNormaler->GetOutput()->GetPointData()->GetArray("Normals");
-    vtkDataArray *tCellNormals =
-      tmpNormaler->GetOutput()->GetCellData()->GetArray("Normals");
-
-    vtkNew(vtkPolyData, smoothedPd);
-    smoothedPd->DeepCopy(smoother->GetOutput());
-
-    vtkNew(vtkPolyDataNormals, smoothNormaler);
-    smoothNormaler->SetInputData(smoothedPd);
-    smoothNormaler->SplittingOff();
-    smoothNormaler->ComputeCellNormalsOn();
-    smoothNormaler->ComputePointNormalsOn();
-    smoothNormaler->Update();
-
-    vtkDataArray *sPtNormals =
-      smoothNormaler->GetOutput()->GetPointData()->GetArray("Normals");
-    vtkDataArray *sCellNormals =
-      smoothNormaler->GetOutput()->GetCellData()->GetArray("Normals");
-
-    int numPoints = tmp->GetNumberOfPoints();
-
-    smoothedPd->BuildLinks();
-
-    double maxAngle = -1.0;
-    double prevMaxAngle = 95.0;
-    for (int i=0; i<numPoints; i++)
+    if (this->SourcePd != NULL)
     {
-      if (fixedPoints[i])
-      {
-        continue;
-      }
+      int numPts = tmp->GetNumberOfPoints();
+      int subId;
+      double smoothPt[3];
+      double closestPt[3], distance;
+      vtkIdType closestCellId;
+      vtkNew(vtkGenericCell, genericCell);
 
+      double moveDot;
       double oNormal[3];
-      oPtNormals->GetTuple(i, oNormal);
-
-      double tNormal[3];
-      tPtNormals->GetTuple(i, tNormal);
-
-      double sNormal[3];
-      sPtNormals->GetTuple(i, sNormal);
-
-      double oPt[3];
-      this->WorkPd->GetPoint(i, oPt);
-
-      double tPt[3];
-      tmp->GetPoint(i, tPt);
-
-      double sPt[3];
-      smoothedPd->GetPoint(i, sPt);
-
-      // ======================OLD ALPHA BETA CODE==========================
-      //double oVec[3];
-      //for (int j=0; j<3; j++)
-      //  oVec[j] = sPt[j] - (this->Alpha * oPt[j] + ((1 - this->Alpha) * tPt[j]));
-
-      //double normalDot = vtkMath::Dot(sNormal, oVec);
-      //vtkMath::MultiplyScalar(sNormal, normalDot);
-
-      //double tangVec[3];
-      //vtkMath::Subtract(oVec, sNormal, tangVec);
-      // ======================OLD ALPHA BETA CODE==========================
-
-      // ======================TRYING NEIGHBOR AVG=========================
-
-      // POINT AVERAGE
-      int numNeighs = 0;
-      double avgAngleDiff = 0;
-      double neighborNormal[3];
-      std::vector<double> neighborAngleDiffs(connectedPointIds[i].size());
-      for (int j=0; j<connectedPointIds[i].size(); j++)
-      {
-        if (fixedPoints[connectedPointIds[i][j]])
-        {
-          continue;
-        }
-
-        tPtNormals->GetTuple(connectedPointIds[i][j], neighborNormal);
-
-        neighborAngleDiffs[j] = vtkMath::AngleBetweenVectors(tNormal, neighborNormal);
-
-        avgAngleDiff += neighborAngleDiffs[j];
-
-        numNeighs++;
-      }
-
-      if (numNeighs != 0)
-      {
-        avgAngleDiff /= numNeighs;
-      }
-      else
-      {
-        avgAngleDiff = 0.0;
-      }
-
-      // =============================STD DEV===============================
-      //double stdDevAngleDiff = 0;
-      //for (int j=0; j<connectedPointIds[i].size(); j++)
-      //{
-      //  stdDevAngleDiff += std::pow(neighborAngleDiffs[j] - avgAngleDiff, 2);
-      //}
-
-      //stdDevAngleDiff = std::sqrt(stdDevAngleDiff / connectedPointIds[i].size());
-
-      //fprintf(stdout,"AVG ANGLE %.6f\n", 180.0*avgAngleDiff/SV_PI);
-      //fprintf(stdout,"STD DEV ANGLE %.6f\n", 180.0*stdDevAngleDiff/SV_PI);
-      // =============================STD DEV===============================
-
-      //// CELL AVERAGE
-      //double avgAngleDiff = 0;
-      //double neighborNormal[3];
-      //std::vector<double> neighborAngleDiffs(connectedCellIds[i].size());
-      //for (int j=0; j<connectedCellIds[i].size(); j++)
-      //{
-      //  tCellNormals->GetTuple(connectedCellIds[i][j], neighborNormal);
-
-      //  neighborAngleDiffs[j] = vtkMath::AngleBetweenVectors(tNormal, neighborNormal);
-
-      //  avgAngleDiff += neighborAngleDiffs[j];
-      //}
-
-      //avgAngleDiff /= connectedCellIds[i].size();
-
-
-      double sVec[3];
-      vtkMath::Subtract(sPt, tPt, sVec);
-
-      double normalDot = vtkMath::Dot(tNormal, sVec);
-      vtkMath::MultiplyScalar(tNormal, normalDot);
-
-      double tangVec[3];
-      vtkMath::Subtract(sVec, tNormal, tangVec);
-
-      // ======================OLD ALPHA BETA CODE==========================
-      //double addVec[3];
-      //for (int j=0; j<3; j++)
-      //  addVec[j] = this->Beta * oVec[j] + ((1 - this->Beta) * oNormal[j]);
-
-      //double finalPt[3];
-      //for (int j=0; j<3; j++)
-      //  finalPt[j] = sPt[j] - addVec[j];
-      // ======================OLD ALPHA BETA CODE==========================
-
-      double tangPt[3];
-      vtkMath::Add(tPt, tangVec, tangPt);
-
-      avgAngleDiff = avgAngleDiff * 180.0/SV_PI;
-      if (avgAngleDiff > maxAngle)
-        maxAngle = avgAngleDiff;
-
-      double lowStop = 10.0;
-      double highStop = 30.0;
-
-      if (avgAngleDiff > highStop)
-        avgAngleDiff = highStop;
-      if (avgAngleDiff < lowStop)
-        avgAngleDiff = lowStop;
-
-      double moveValue = (avgAngleDiff-lowStop)/(highStop - lowStop);
-
       double moveVec[3];
-      vtkMath::Subtract(sPt, tangPt, moveVec);
+      for (int j=0; j<numPts; j++)
+      {
+        tmp->GetPoint(j, smoothPt);
+        cellLocator->FindClosestPoint(smoothPt, closestPt, genericCell, closestCellId, subId, distance);
+        vtkMath::Subtract(smoothPt, closestPt, moveVec);
+        vtkMath::Normalize(moveVec);
 
-      vtkMath::MultiplyScalar(moveVec, moveValue);
+        oPtNormals->GetTuple(j, oNormal);
 
-      double finalPt[3];
-      vtkMath::Add(tangPt, moveVec, finalPt);
-
-      tmp->GetPoints()->SetPoint(i, finalPt);
-
+        moveDot = vtkMath::Dot(oNormal, moveVec);
+        if (moveDot < 0)
+        {
+          tmp->GetPoints()->SetPoint(j, smoothPt);
+        }
+        else
+        {
+          tmp->GetPoints()->SetPoint(j, closestPt);
+        }
+      }
     }
 
-    fprintf(stdout,"MAX ANGLE DIFF: %.6f\n", maxAngle);
-    if (maxAngle > prevMaxAngle)
-    {
-      tmp->DeepCopy(savePd);
-      break;
-    }
+    //vtkNew(vtkPolyDataNormals, tmpNormaler);
+    //tmpNormaler->SetInputData(tmp);
+    //tmpNormaler->SplittingOff();
+    //tmpNormaler->ComputeCellNormalsOn();
+    //tmpNormaler->ComputePointNormalsOn();
+    //tmpNormaler->Update();
 
-    prevMaxAngle = maxAngle;
+    //vtkDataArray *tPtNormals =
+    //  tmpNormaler->GetOutput()->GetPointData()->GetArray("Normals");
+    //vtkDataArray *tCellNormals =
+    //  tmpNormaler->GetOutput()->GetCellData()->GetArray("Normals");
+
+    //vtkNew(vtkPolyData, smoothedPd);
+    //smoothedPd->DeepCopy(smoother->GetOutput());
+
+    //vtkNew(vtkPolyDataNormals, smoothNormaler);
+    //smoothNormaler->SetInputData(smoothedPd);
+    //smoothNormaler->SplittingOff();
+    //smoothNormaler->ComputeCellNormalsOn();
+    //smoothNormaler->ComputePointNormalsOn();
+    //smoothNormaler->Update();
+
+    //vtkDataArray *sPtNormals =
+    //  smoothNormaler->GetOutput()->GetPointData()->GetArray("Normals");
+    //vtkDataArray *sCellNormals =
+    //  smoothNormaler->GetOutput()->GetCellData()->GetArray("Normals");
+
+    //int numPoints = tmp->GetNumberOfPoints();
+
+    //smoothedPd->BuildLinks();
+
+    //double maxAngle = -1.0;
+    //double prevMaxAngle = 95.0;
+    //for (int i=0; i<numPoints; i++)
+    //{
+    //  if (fixedPoints[i])
+    //  {
+    //    continue;
+    //  }
+
+    //  double oNormal[3];
+    //  oPtNormals->GetTuple(i, oNormal);
+
+    //  double tNormal[3];
+    //  tPtNormals->GetTuple(i, tNormal);
+
+    //  double sNormal[3];
+    //  sPtNormals->GetTuple(i, sNormal);
+
+    //  double oPt[3];
+    //  this->WorkPd->GetPoint(i, oPt);
+
+    //  double tPt[3];
+    //  tmp->GetPoint(i, tPt);
+
+    //  double sPt[3];
+    //  smoothedPd->GetPoint(i, sPt);
+
+    //  // ======================OLD ALPHA BETA CODE==========================
+    //  //double oVec[3];
+    //  //for (int j=0; j<3; j++)
+    //  //  oVec[j] = sPt[j] - (this->Alpha * oPt[j] + ((1 - this->Alpha) * tPt[j]));
+
+    //  //double normalDot = vtkMath::Dot(sNormal, oVec);
+    //  //vtkMath::MultiplyScalar(sNormal, normalDot);
+
+    //  //double tangVec[3];
+    //  //vtkMath::Subtract(oVec, sNormal, tangVec);
+    //  // ======================OLD ALPHA BETA CODE==========================
+
+    //  // ======================TRYING NEIGHBOR AVG=========================
+
+    //  // POINT AVERAGE
+    //  int numNeighs = 0;
+    //  double avgAngleDiff = 0;
+    //  double neighborNormal[3];
+    //  std::vector<double> neighborAngleDiffs(connectedPointIds[i].size());
+    //  for (int j=0; j<connectedPointIds[i].size(); j++)
+    //  {
+    //    if (fixedPoints[connectedPointIds[i][j]])
+    //    {
+    //      continue;
+    //    }
+
+    //    tPtNormals->GetTuple(connectedPointIds[i][j], neighborNormal);
+
+    //    neighborAngleDiffs[j] = vtkMath::AngleBetweenVectors(tNormal, neighborNormal);
+
+    //    avgAngleDiff += neighborAngleDiffs[j];
+
+    //    numNeighs++;
+    //  }
+
+    //  if (numNeighs != 0)
+    //  {
+    //    avgAngleDiff /= numNeighs;
+    //  }
+    //  else
+    //  {
+    //    avgAngleDiff = 0.0;
+    //  }
+
+    //  // =============================STD DEV===============================
+    //  //double stdDevAngleDiff = 0;
+    //  //for (int j=0; j<connectedPointIds[i].size(); j++)
+    //  //{
+    //  //  stdDevAngleDiff += std::pow(neighborAngleDiffs[j] - avgAngleDiff, 2);
+    //  //}
+
+    //  //stdDevAngleDiff = std::sqrt(stdDevAngleDiff / connectedPointIds[i].size());
+
+    //  //fprintf(stdout,"AVG ANGLE %.6f\n", 180.0*avgAngleDiff/SV_PI);
+    //  //fprintf(stdout,"STD DEV ANGLE %.6f\n", 180.0*stdDevAngleDiff/SV_PI);
+    //  // =============================STD DEV===============================
+
+    //  //// CELL AVERAGE
+    //  //double avgAngleDiff = 0;
+    //  //double neighborNormal[3];
+    //  //std::vector<double> neighborAngleDiffs(connectedCellIds[i].size());
+    //  //for (int j=0; j<connectedCellIds[i].size(); j++)
+    //  //{
+    //  //  tCellNormals->GetTuple(connectedCellIds[i][j], neighborNormal);
+
+    //  //  neighborAngleDiffs[j] = vtkMath::AngleBetweenVectors(tNormal, neighborNormal);
+
+    //  //  avgAngleDiff += neighborAngleDiffs[j];
+    //  //}
+
+    //  //avgAngleDiff /= connectedCellIds[i].size();
+
+
+    //  double sVec[3];
+    //  vtkMath::Subtract(sPt, tPt, sVec);
+
+    //  double normalDot = vtkMath::Dot(tNormal, sVec);
+    //  vtkMath::MultiplyScalar(tNormal, normalDot);
+
+    //  double tangVec[3];
+    //  vtkMath::Subtract(sVec, tNormal, tangVec);
+
+    //  // ======================OLD ALPHA BETA CODE==========================
+    //  //double addVec[3];
+    //  //for (int j=0; j<3; j++)
+    //  //  addVec[j] = this->Beta * oVec[j] + ((1 - this->Beta) * oNormal[j]);
+
+    //  //double finalPt[3];
+    //  //for (int j=0; j<3; j++)
+    //  //  finalPt[j] = sPt[j] - addVec[j];
+    //  // ======================OLD ALPHA BETA CODE==========================
+
+    //  double tangPt[3];
+    //  vtkMath::Add(tPt, tangVec, tangPt);
+
+    //  avgAngleDiff = avgAngleDiff * 180.0/SV_PI;
+    //  if (avgAngleDiff > maxAngle)
+    //    maxAngle = avgAngleDiff;
+
+    //  double lowStop = 10.0;
+    //  double highStop = 30.0;
+
+    //  if (avgAngleDiff > highStop)
+    //    avgAngleDiff = highStop;
+    //  if (avgAngleDiff < lowStop)
+    //    avgAngleDiff = lowStop;
+
+    //  double moveValue = (avgAngleDiff-lowStop)/(highStop - lowStop);
+
+    //  double moveVec[3];
+    //  vtkMath::Subtract(sPt, tangPt, moveVec);
+
+    //  vtkMath::MultiplyScalar(moveVec, moveValue);
+
+    //  double finalPt[3];
+    //  vtkMath::Add(tangPt, moveVec, finalPt);
+
+    //  tmp->GetPoints()->SetPoint(i, finalPt);
+
+    //}
+
+    //fprintf(stdout,"MAX ANGLE DIFF: %.6f\n", maxAngle);
+    //if (maxAngle > prevMaxAngle)
+    //{
+    //  tmp->DeepCopy(savePd);
+    //  break;
+    //}
+
+    //prevMaxAngle = maxAngle;
   }
 
   output->DeepCopy(tmp);
